6bd7b425a59d502229e2033db82b32164d802604
Fede154145 <guesdonfederico54@gmail.com>
2024-02-09 09:56:15 -0300

    ej2 if resuelto

--- 00-Unidades/02_if/Ejercicios_If/IF_app_02.py
+++ 00-Unidades/02_if/Ejercicios_If/IF_app_02.py
@@ -33,9 +33,10 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
+        edad = int(self.txt_edad.get())
 
-        
+        if edad > 17:
+            alert("Mensaje", "Mayor")
         
     
 if __name__ == "__main__":
